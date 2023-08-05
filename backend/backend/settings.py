e93220522536fb6565a3717acee516da2ae2bb28
Alexander Karelin <karelin888@gmail.com>
2023-08-05 21:37:04 +0300

    deploy review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 RECIPESR_ON_PAGE = 6
